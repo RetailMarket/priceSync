34570cc569e159a8e26a2cbd628b8336ce1600c7
Akshay <kumarsharmaa69@gmail.com>
2017-01-25 16:08:23 +0530

    Running job from main, currently run job at every 10s

--- app/main/main.go
+++ app/main/main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	clients "Retail/priceSync/clients"
-	"Retail/priceSync/database"
 	"Retail/priceSync/app/jobs"
 )
 
 func main() {
-	database.Init();
-	defer database.CloseDb();
-
 	clients.CreateClientConnection()
-	clients.CloseConnections();
+	defer clients.CloseConnections();
 
 	// running job for sending update price record for approval.
 	jobs.SendUpdatePriceForApprovalJob()
-
 }
